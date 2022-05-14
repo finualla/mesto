f6b9a927ee7ce0dc633ee67cb16a7156bfa2ce3b
Bella B <bellabzhukova@MacBook-Pro-Bella.local>
2022-05-14 18:42:47 +0300

    js submit fixed, header at 320 fixed

--- vendor/index.js
+++ vendor/index.js
@@ -2,11 +2,17 @@ const openButton = document.querySelector('.button-edit');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.button-close');
 const submitButton = document.querySelector('.button-submit');
-const formElement = document.querySelector(".popup__form");
+const formElement = popup.querySelector(".popup__form");
+let nameInput = popup.querySelector('.popup__input_value_name');
+let jobInput = popup.querySelector('.popup__input_value_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
 
 // Открытие popup
 function openPopup() {
     popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 }
 openButton.addEventListener('click', openPopup);
 
@@ -19,16 +25,8 @@ closeButton.addEventListener('click', closePopup);
 // Обработчик отправки формы
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    // Получите значение полей jobInput и nameInput из свойства value
-    let nameInput = document.querySelector('.popup__input_value_name');
-    let jobInput = document.querySelector('.popup__input_value_job');
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__job');
-    // Вставьте новые значения с помощью textContent
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     closePopup();
 }
-// Прикрепляем обработчик к форме
 formElement.addEventListener('submit', formSubmitHandler); 
